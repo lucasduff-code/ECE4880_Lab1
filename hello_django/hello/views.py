29ff463b9c42d6d7804eab32c8676545fba961aa
lucasduff-code <lucasjduff@gmail.com>
2021-09-13 18:28:22 -0500

    added whole base framework django file structure

---
+++ hello_django/hello/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.utils.timezone import datetime
+import re
+from django.shortcuts import render
+
+# Replace the existing home function with the one below
+def home(request):
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
