f4f7bf225cf186fb03b683d8a7616177ae77fe60
lucasduff-code <lucasjduff@gmail.com>
2021-09-22 18:26:20 -0500

    added framwork for the notifications settings

--- hello_django/env/bin/activate.fish
+++ hello_django/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lucasduff/Desktop/Senior Design/hello_django/env"
+set -gx VIRTUAL_ENV "/Users/lucasduff/git/ECE4880_Lab1/hello_django/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
